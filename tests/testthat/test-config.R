d71f7b188b5d5955a724a313aebfe229d3eccd57
iofarm <ian.farm@maine.edu>
2026-03-19 14:21:50 -0400

    Add unset argument to git_config_set()

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -12,3 +12,22 @@ test_that("local, custom config roundtrip", {
   cfg <- git_config(repo)
   expect_equal(cfg$value[cfg$name == "aaa.bbb"], character())
 })
+
+test_that("multivar, local, custom config roundtrip", {
+  repo <- git_init(tempfile("gert-tests-config-multivar"))
+  on.exit(unlink(repo, recursive = TRUE))
+
+  git_config_set("aaa.bbb", "ccc", repo = repo, add = TRUE)
+  git_config_set("aaa.bbb", "ddd", repo = repo, add = TRUE)
+  git_config_set("aaa.bbb", "eee", repo = repo, add = TRUE)
+  cfg <- git_config(repo)
+  expect_equal(cfg$value[cfg$name == "aaa.bbb"], c("ccc", "ddd", "eee"))
+
+  git_config_set("aaa.bbb", "ddd", repo = repo, unset = TRUE)
+  cfg <- git_config(repo)
+  expect_equal(cfg$value[cfg$name == "aaa.bbb"], c("ccc", "eee"))
+
+  git_config_set("aaa.bbb", "^[a-z]{3}$", repo = repo, unset = TRUE)
+  cfg <- git_config(repo)
+  expect_equal(cfg$value[cfg$name == "aaa.bbb"], character())
+})
